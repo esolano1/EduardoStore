0a0294ded1cb1054d71f95a0c81ac4fc5c3c9d85
esolano1 <joeybello23@gmail.com>
2025-10-06 19:53:17 -0600

    Saving some content

---
+++ burger-house/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  // dark mode por clase (como ya usas `dark:` en el body)
+  darkMode: "class",
+  theme: { extend: {} },
+  plugins: [],
+}
